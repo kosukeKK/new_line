f5c3f8d400672b5130358d88bccc0a03bd0e0e0a
kosukeKK <kosuke.kato@jmty.jp>
2017-12-06 22:31:35 +0900

    quote統一

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var server = require("http").createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/json" });
+var server = require('http').createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/json' });
 }).listen(3001);
-var io = require("socket.io").listen(server);
+var io = require('socket.io').listen(server);
 console.log('Listen to the stereo tonight! tonight! tonight!')
 
-io.sockets.on("connection", function (socket) {
-    socket.on("publish", function (data) {
-        io.sockets.emit("publish", { value: data.value });
+io.sockets.on('connection', function (socket) {
+    socket.on('publish', function (data) {
+        io.sockets.emit('publish', { value: data.value });
     });
 });
